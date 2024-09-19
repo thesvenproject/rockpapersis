53e8fab016727222358129672a121e18d6e7d22b
thesvenproject <176626745+thesvenproject@users.noreply.github.com>
2024-09-20 01:00:36 +0200

    rockpaper

--- javascript.js
+++ javascript.js
@@ -4,66 +4,114 @@
 
 let number = Math.random() * 100;
 let humanChoice = prompt("rock paper or scissor");
+humanChoice = humanChoice.toLowerCase();
 let computerChoice = getComputerChoice();
 let humanScore = 0;
 let computerScore = 0;
+let result = playRound();
 
-
-//gets humanChoice value
+/*
+//dit deed eigenlijk helemaal niks
 function getHumanChoice(humanChoice) {
-    if (humanChoice.toLowerCase() == "rock"){
+    if (humanChoice.toLowerCase() === "rock"){
         return "rock";
     }
-    else if (humanChoice.toLowerCase() == "scissor"){
+    else if (humanChoice.toLowerCase() === "scissor"){
         return "scissor";
     }
-    else if (humanChoice.toLowerCase() == "paper"){
+    else if (humanChoice.toLowerCase() ==="paper"){
         return "paper";
     }
 
     else {
-        console.error("error");
+        return ("error");
     }
 }
+*/
 
-console.log(humanChoice);
-
-
-console.log(number);
 //gets computerChoice value
 function getComputerChoice(computerChoice){
-    if (number >= 0 && number <= 33) {
+    if ( number <= 33) {
     return "rock";
     }
     
     else if (number > 33 && number <= 66) {
-    return "scissor"; 
+        return "scissor"; 
     
     }
 
-    else if (number > 66 && number <= 100){
+    else if (number > 66){
         return "paper";
     }
 
     else {
-        console.log("error");
+        return "error";
     }
 }
 
 
-console.log(computerChoice);
 
 // did werkt niet, ik krijg de waardes goed terug maar kan niet comparen
- function playRound(humanChoice, computerChoice){
-    if ("rock" && "rock"){
-        console.log("its a tie!");
+ function playRound(){
+     if (humanChoice === computerChoice){
+       return "its a tie!";
+       
     }
+   
+// verbeterde versie rock paper playround
+
+    else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "scissor" && computerChoice === "rock" || humanChoice === "paper" && computerChoice === "scissor"){
+        computerScore++;
+        //console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);    
+        return "you lost";
+    }
+
     else {
-        console.log("test2");
+            humanScore++;
+            //console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
+            return "you won";
     }
-}
-
-playRound();
 
 
-console.log(humanChoice === computerChoice);
+// oude versie rock paper playround
+   /*
+
+    else if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            computerScore = computerScore + 1;
+            return "you lost";
+        }
+        else {
+            humanScore = humanScore + 1;
+            return "you won";
+        }
+    }
+
+    else if (humanChoice === "paper"){
+        if (computerChoice === "scissor"){
+            computerScore = computerScore + 1;
+            return "you lost";
+        }
+        else {
+            humanScore = humanScore + 1;
+            return "you won";
+        }
+
+        
+    }
+
+    else if (humanChoice === "scissor"){
+        if (computerChoice === "rock"){
+            computerScore = computerScore + 1;
+            return "you lost";  
+        }
+        else {
+            humanScore = humanScore + 1;
+            return "you won";
+        }
+    } */
+};
+
+
+console.log(`You have chosen: ${humanChoice} & Computer has chosen: ${computerChoice}  = ${result}`);
+console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
